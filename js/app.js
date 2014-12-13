630107454f611f6b73b5fe15d99d7150e64a1b14
davidpetri <petridw@gmail.com>
2014-12-12 17:56:12 -0800

    user can click and mark squares, reset game. hover colors in CSS

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@ app = angular.module('ticTacToeApp', []);
 
 app.controller('GameController', ['$scope', function($scope) {
 
-  // $scope.board = [
-  //   ["b", "b", "b"],
-  //   ["b", "b", "b"],
-  //   ["b", "b", "b"]
-  // ];
-
   $scope.board = [];
   $scope.boardSize = 3;
+  $scope.turn = true;
+  $scope.gameInProgress = false;
 
   //whenever boardSize changes, dynamically update the board
   $scope.$watch("boardSize", function() {
+    $scope.makeBoard();
+  });
+
+  $scope.makeBoard = function() {
  
     $scope.board = [];
  
@@ -39,7 +39,31 @@ app.controller('GameController', ['$scope', function($scope) {
         });
       }
     }
-  });
+  };
+
+
+  $scope.clickSquare = function (col) {
+    //start game if it hasn't been started
+    if (!$scope.gameInProgress) {
+      $scope.startGame();
+    }
+
+    //if square isn't marked, mark it & change turns
+    if (!col.filled) {
+      col.filled = true;
+
+      if ($scope.turn) {
+        col.val = "x";
+      } else {
+        col.val = "o";
+      }
+
+    //change turns
+    $scope.turn = !$scope.turn;
+    }
+
+
+  };
 
   $scope.startGame = function() {
     var sizeSlider;
@@ -50,5 +74,16 @@ app.controller('GameController', ['$scope', function($scope) {
     sizeSlider.disabled = true;
   };
 
+  $scope.resetGame = function() {
+    var sizeSlider;
+
+    //enable slider
+    sizeSlider = document.getElementById("size-slider");
+    sizeSlider.disabled = false;
+
+    //make a new board
+    $scope.makeBoard();
+
+  };
 
 }]);
