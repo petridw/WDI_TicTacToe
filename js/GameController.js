1ad31f4dde8e8ce1b9e4c9b009acb41469c1d3a0
davidpetri <petridw@gmail.com>
2014-12-17 17:16:39 -0800

    added firebase logic to create new blank board

--- js/GameController.js
+++ js/GameController.js
@@ -3,72 +3,87 @@ angular
   .controller("GameController", ['$firebase', GameController]);
 
 function GameController($firebase) {
-  var ref = new Firebase("https://petri-tic-tac-toe.firebaseio.com/games");    
+  //------------------------------------------------------------------
+  // Local variables (only in controller's scope)
+  //------------------------------------------------------------------
+  var gamesRef = new Firebase("https://petri-tic-tac-toe.firebaseio.com/games");
 
+  //blank game template
+  var blankBoard = {
+    rows: {  row1: {
+            s1: { val: "", filled: false, winner: false },
+            s2: { val: "", filled: false, winner: false },
+            s3: { val: "", filled: false, winner: false }
+          }, row2: {
+            s1: { val: "", filled: false, winner: false },
+            s2: { val: "", filled: false, winner: false },
+            s3: { val: "", filled: false, winner: false },
+          }, row3: {
+            s1: { val: "", filled: false, winner: false },
+            s2: { val: "", filled: false, winner: false },
+            s3: { val: "", filled: false, winner: false },
+          }},
+    turn: "x",
+    winner: "",
+    gameInProgress: false,
+    postGame: false
+  };
 
-  function getBoard() {
-    var games = $firebase(ref).$asArray();
+  //------------------------------------------------------------------
+  // Properties available to the View
+  //------------------------------------------------------------------
+  this.board = {};
 
-    //add new game, once it's added get the key
-    var gameKey;
+  //------------------------------------------------------------------
+  // Functions available to the View
+  //------------------------------------------------------------------
+  this.resetBoard = resetBoard;
 
-    games.$add({
-      rows: {row1: {
-              s1: {
-                val: "",
-                filled: false,
-                winner: false
-              },
-              s2: {
-                val: "",
-                filled: false,
-                winner: false
-              },
-              s3: {
-                val: "",
-                filled: false,
-                winner: false
-              }
-            }, row2: {
-              s1: {
-                val: "",
-                filled: false,
-                winner: false
-              },
-              s2: {
-                val: "",
-                filled: false,
-                winner: false
-              },
-              s3: {
-                val: "",
-                filled: false,
-                winner: false
-              }
-            }, row3: {
-              s1: {
-                val: "",
-                filled: false,
-                winner: false
-              },
-              s2: {
-                val: "",
-                filled: false,
-                winner: false
-              },
-              s3: {
-                val: "",
-                filled: false,
-                winner: false
-              }
-            }},
-      boardSize: 3,
-      turn: true,
-      winner: "",
-      gameInProgress: false,
-      postGame: false
-    }).then(function(ref){
-      gameKey = ref.key();
-    });
+  //------------------------------------------------------------------
+  // Functions
+  //------------------------------------------------------------------
+
+  //******************************************************
+  // Add a new game board to the /games/ firebase 
+  //******************************************************
+  function getNewBoard() {
+    console.log("in getNewBoard...");
+
+    var games = $firebase(gamesRef).$asArray();
+
+    //once games is loaded
+    games.$loaded().then(function(ref) {
+      games.$add(blankBoard).then(function(ref) {
+        var key = ref.key();
+        console.log("added record with key: " + key);
+
+        this.board = games.$getRecord(key);
+        console.log("have new board: " + this.board);
+
+      }, catchError);
+    }, catchError);
+
+    var newestGame = games[games.length];
+
+    console.log("newest game: " + newestGame);
   }
+
+  function resetBoard() {
+    console.log("in resetBoard()");
+
+    this.board = getNewBoard();
+  }
+
+  function catchError(error) {
+    console.error("in catchError: ", error);
+  }
+
+
+
+
+
+
+
+
+
 }
